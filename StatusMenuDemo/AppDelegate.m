a53592eefc7ae5d8197d3870ba5088797a06ef7f
Saori Kohno <saorikohno@yahoo.com>
2016-11-13 22:58:29 +0900

    finish

--- StatusMenuDemo/AppDelegate.m
+++ StatusMenuDemo/AppDelegate.m
@@ -8,15 +8,19 @@
 
 #import "AppDelegate.h"
 
-@interface AppDelegate ()
+@interface AppDelegate (){
+    IBOutlet NSMenu *stMenu;
+}
 
 @property (weak) IBOutlet NSWindow *window;
 @end
 
-@implementation AppDelegate
+@implementation AppDelegate{
+    NSStatusItem* _stItem;
+}
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
-    // Insert code here to initialize your application
+    [self setUpStMenu];
 }
 
 
@@ -24,5 +28,19 @@
     // Insert code here to tear down your application
 }
 
+//ステータスメニュー項目を設定
+- (void)setUpStMenu{
+    NSStatusBar *sysStBar = [NSStatusBar systemStatusBar];
+    _stItem = [sysStBar statusItemWithLength:NSVariableStatusItemLength];
+    [_stItem setHighlightMode:YES];
+    [_stItem setImage:[NSImage imageNamed:@"stIcon.png"]];
+    [_stItem setAlternateImage:[NSImage imageNamed:@"stIcon_hl.png"]];
+    [_stItem setMenu:stMenu];
+}
+
+//メニューアクション
+- (IBAction)ShowMainWin:(id)sender{
+    [self.window makeKeyAndOrderFront:self];
+}
 
 @end
