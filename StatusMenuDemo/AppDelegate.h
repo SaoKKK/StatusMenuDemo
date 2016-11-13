003d2a53f04ce792ae32d1d3ec5ecbb71b265d02
Saori Kohno <saorikohno@yahoo.com>
2016-11-13 14:43:33 +0900

    Initial Commit

---
+++ StatusMenuDemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  StatusMenuDemo
+//
+//  Created by 河野 さおり on 2016/11/13.
+//  Copyright © 2016年 Saori Kohno. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ StatusMenuDemo/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  StatusMenuDemo
+//
+//  Created by 河野 さおり on 2016/11/13.
+//  Copyright © 2016年 Saori Kohno. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
